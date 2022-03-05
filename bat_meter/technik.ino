f78727cefbd93abe3a93a0f2af752eb93b635832
TheEinfachFelix <74210839+TheEinfachFelix@users.noreply.github.com>
2022-03-05 19:43:38 +0100

    Fixed kompiler eror and bugs

--- bat_meter/technik.ino
+++ bat_meter/technik.ino
@@ -1,39 +1,43 @@
 int Lampen[10] = {12,11,10,9,8,7,6,5,4,3};
 char input = A0;
 int out;
-string debug = "";
+String debug = "";
 
-void setup() {  
+#define ELEMENTCOUNT(x)  (sizeof(x) / sizeof(x[0]))
+
+void setup() {
   Serial.begin(9600);
 //pins Definieren
   pinMode(input, INPUT);
 // output pins klug Definieren
   int a = 0;
-  while(a<sizeof(Lampen)){
-    pinMode(Lampen[a], OUTPUT);
+for(int index = 0; index < ELEMENTCOUNT(Lampen); index++){
+    pinMode(Lampen[index], OUTPUT);
     a++;
   };
-  Serial.println("OK Lets go!")
+  Serial.println("OK Lets go!");
 }
 
 void loop() {
-  int g = analogRead(input);
-  debug = debug + "v: " + g;  
-  out = map(g,0,1000,0,10);
-  debug = debug + ",map: " + out + ",LEDs: "
-    
+  int input = analogRead(input);
+  debug = debug + "v: " + input;
+  out = map(input,0,1000,0,10);
+  debug = debug + ",map: " + out + ",LEDs: ";
+
 //leds an machen
   int f = 0;
   while(f<out){
-    debug = debug + "1|"
+    debug = debug + "1|";
     digitalWrite(Lampen[f], HIGH);
     f++;
   };
-//rest aus machen 
+//rest aus machen
   while(f<10){
-    debug = debug + "0|"
+    debug = debug + "0|";
     digitalWrite(Lampen[f], LOW);
     f++;
-  }; 
-  Serial.println(debug)
+  };
+  delay(50);
+  Serial.println(debug);
+  debug = "";
 }
